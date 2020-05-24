8adc616ec89d9d9e6143cd645a4fe98c361a7f52
KelvinMitaki <mitakikelvin1@gmail.com>
2020-05-24 18:16:59 +0300

    added filtering range by ages in the DB

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -1,4 +1,4 @@
-const Artist = require('../models/artist');
+const Artist = require("../models/artist");
 
 /**
  * Finds the lowest and highest age of artists in the Artist collection
@@ -6,4 +6,16 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+  const min = Artist.find({})
+    .sort({ age: 1 })
+    .limit(1)
+    .then(artists => artists[0].age);
+  const max = Artist.find({})
+    .sort({ age: -1 })
+    .limit(1)
+    .then(artists => artists[0].age);
+  return Promise.all([min, max]).then(ages => ({
+    min: ages[0],
+    max: ages[1]
+  }));
 };
