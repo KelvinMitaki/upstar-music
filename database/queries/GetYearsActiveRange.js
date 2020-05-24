67b519127783e61c147611d78116ec7b39ee360e
KelvinMitaki <mitakikelvin1@gmail.com>
2020-05-24 18:27:05 +0300

    added filtering range by yearsActive in the DB

--- database/queries/GetYearsActiveRange.js
+++ database/queries/GetYearsActiveRange.js
@@ -1,4 +1,4 @@
-const Artist = require('../models/artist');
+const Artist = require("../models/artist");
 
 /**
  * Finds the lowest and highest yearsActive of artists in the Artist collection
@@ -6,4 +6,16 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+  const min = Artist.find({})
+    .sort({ yearsActive: 1 })
+    .limit(1)
+    .then(artists => artists[0].yearsActive);
+  const max = Artist.find({})
+    .sort({ yearsActive: -1 })
+    .limit(1)
+    .then(artists => artists[0].yearsActive);
+  return Promise.all([min, max]).then(active => ({
+    min: active[0],
+    max: active[1]
+  }));
 };
