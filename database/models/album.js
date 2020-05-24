884fd17b4b5048e2ef5cce7056391f83725b385c
KelvinMitaki <mitakikelvin1@gmail.com>
2020-05-24 15:15:26 +0300

    added artists and album schemas

--- database/models/album.js
+++ database/models/album.js
@@ -1,1 +1,13 @@
 // Todo: create Album Schema
+const mongoose = require("mongoose");
+
+const AlbumSchema = new mongoose.Schema({
+  title: String,
+  date: Date,
+  copiesSold: Number,
+  numberTracks: Number,
+  image: String,
+  revenue: Number
+});
+const Album = mongoose.model("Album", AlbumSchema);
+module.exports = Album;
