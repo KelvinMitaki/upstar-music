58cc9d74015259db62dc9b46717e715905d52c2e
KelvinMitaki <mitakikelvin1@gmail.com>
2020-05-25 09:11:13 +0300

    added search filtering by name, age and yearsActive

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -1,4 +1,4 @@
-const Artist = require('../models/artist');
+const Artist = require("../models/artist");
 
 /**
  * Searches through the Artist collection
@@ -9,4 +9,29 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+  const Obj = {};
+  if (criteria.name.length !== 0) {
+    Obj.name = criteria.name;
+  }
+  if (criteria.age) {
+    const { max, min } = criteria.age;
+    Obj.age = { $gte: min, $lte: max };
+  }
+  if (criteria.yearsActive) {
+    const { max, min } = criteria.yearsActive;
+    Obj.yearsActive = { $gte: min, $lte: max };
+  }
+
+  const artists = Artist.find(Obj)
+    .sort({ [sortProperty]: 1 })
+    .skip(offset)
+    .limit(limit)
+    .then(artists => artists);
+  console.log(artists);
+  return Promise.all([artists, Artist.count()]).then(result => ({
+    all: result[0],
+    count: result[1],
+    offset,
+    limit
+  }));
 };
